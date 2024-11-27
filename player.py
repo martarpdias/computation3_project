d1dbdb8b700bebada3a3d3a44a764a30ed967fa6
martarpdias <20231642@novaims.unl.pt>
2024-11-27 16:02:24 +0000

    mudar setas

---
+++ player.py
@@ -0,0 +1,63 @@
+from config import *
+import pygame
+import math
+from bullet import Bullet
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        """
+        Initialize the player instance
+        """
+        super().__init__()
+        self.image = pygame.Surface(player_size)
+        self.image.fill(blue)
+        self.rect=self.image.get_rect()
+        self.rect.center=(width//2,height//2)
+
+
+        #Gameplay variables
+        self.speed=5
+        self.health=100
+        self.bullet_cooldown=0
+
+    def update(self):
+        """
+        Update the +position of the player based on keyboard input
+        """
+        keys=pygame.key.get_pressed()
+        #Moving upwards
+        if (keys[pygame.K_UP] or keys[pygame.K_w]) and self.rect.top>0:
+            self.rect.y-= self.speed
+        #Moving downwards
+        if (keys[pygame.K_DOWN] or keys[pygame.K_s]) and self.rect.bottom<height:
+            self.rect.y+= self.speed
+        #Moving left
+        if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and self.rect.left>0:
+            self.rect.x-= self.speed
+        #Moving right
+        if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and self.rect.right<width:
+            self.rect.x+= self.speed
+
+
+    def shoot(self,bullets:pygame.sprite.Group):
+        """
+        Shoot bullets in 4 directions depending om«n cooldown.
+
+        Args
+        ---
+        bullets (pygame.sprite.Group)
+            The bullet group that we will add the news ones to
+        ---
+        """
+        if self.bullet_cooldown<=0:
+            for angle in [0, math.pi / 2, math.pi, 3 * math.pi / 2]:
+                bullet=Bullet(
+                    self.rect.center[0],self.rect.center[1], angle
+                )
+                bullets.add(bullet)
+            self.bullet_cooldown=fps #frames until the next shot
+        self.bullet_cooldown -=1
+
+
+
+
